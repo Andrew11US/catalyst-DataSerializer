39caab60ac41dba20852e5de4bbb621d50831fca
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-10-25 00:49:47 +0200

    Initial Commit

---
+++ DataSerializer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataSerializer
+//
+//  Created by Andrew on 10/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
